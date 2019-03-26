32053216de49ba11398161c8f19eeec3d4e8ec7e
Pawel Oczkowski <pawel.oczko84@gmail.com>
2019-03-26 09:52:01 +0100

    Init Project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import * as serviceWorker from './serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(<App/>, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import DevTools from "./DevTools";
+import { getCountries } from "./actions/actions-countries";
+
+render(
+  <Provider store={store}>
+    <div>
+      <h1>Inicjalizacja projektu</h1>
+      <DevTools />
+    </div>
+  </Provider>,
+  document.getElementById("root")
+);
+store.dispatch(getCountries());
